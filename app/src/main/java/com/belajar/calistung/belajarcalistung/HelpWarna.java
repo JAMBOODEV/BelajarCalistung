f22f8cdd5276da86f4b835db4ae4e7476387db6d
inudola <inudola@gmail.com>
2016-11-18 16:03:23 +0700

    Add Help Menu

---
+++ app/src/main/java/com/belajar/calistung/belajarcalistung/HelpWarna.java
@@ -0,0 +1,13 @@
+package com.belajar.calistung.belajarcalistung;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HelpWarna extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_help_warna);
+    }
+}
